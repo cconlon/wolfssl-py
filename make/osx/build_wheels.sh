6f96ae11c916a67686ef33ae275c1176a2f1e422
Chris Conlon <chris@wolfssl.com>
2019-01-22 16:55:45 -0700

    upgrade pip after download

--- make/osx/build_wheels.sh
+++ make/osx/build_wheels.sh
@@ -5,6 +5,7 @@ for PYVERSION in 2.7 3.4 3.5 3.6; do
     virtualenv -p /Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin/python${PYVERSION} venv_${PYVERSION}
     . ./venv_${PYVERSION}/bin/activate
     curl https://bootstrap.pypa.io/get-pip.py | python
+    pip install --upgrade pip
     python setup.py bdist_wheel
     pip install -r requirements/test.txt
     set +e
