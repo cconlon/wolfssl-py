df69607f5de0a24fd3d1334362624f7528ba0a72
Chris Conlon <chris@wolfssl.com>
2019-01-23 09:47:28 -0700

    travis fix

--- make/osx/build_wheels.sh
+++ make/osx/build_wheels.sh
@@ -2,15 +2,18 @@ set -e
 set +x
 
 for PYVERSION in 2.7 3.4 3.5 3.6; do
-    virtualenv -p /Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin/python${PYVERSION} venv_${PYVERSION}
-    . ./venv_${PYVERSION}/bin/activate
+    PYTHONEXE=/Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin/python${PYVERSION}
 
-    # update pip
+    # update pip for newer TLS support
     curl https://bootstrap.pypa.io/get-pip.py | python
     pip install --upgrade pip
 
-    # update setuptools
-    pip install --upgrade setuptools
+    # update virtualenv and setuptools
+    $PYTHONEXE -m pip install virtualenv
+    $PYTHONEXE pip install --upgrade setuptools
+
+    virtualenv -p /Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin/python${PYVERSION} venv_${PYVERSION}
+    . ./venv_${PYVERSION}/bin/activate
 
     python setup.py bdist_wheel
     pip install -r requirements/test.txt
