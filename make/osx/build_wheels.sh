675dcb56f90f688cc2ec4013bb4f3ca4af240104
Chris Conlon <chris@wolfssl.com>
2019-01-23 14:01:31 -0700

    try homebrew for python2/3

--- make/osx/build_wheels.sh
+++ make/osx/build_wheels.sh
@@ -6,15 +6,19 @@ for PYVERSION in 2.7 3.4 3.5 3.6; do
     PYTHONEXE=${PYTHONBIN}/python${PYVERSION}
     export PATH="${PYTHONBIN}":$PATH
 
+    # update pip for newer TLS support
+    #curl https://bootstrap.pypa.io/get-pip.py | ${PYTHONEXE}
+    #"${PYTHONBIN}/pip" install --upgrade pip
+
+    brew update
+    brew install python2
+    brew install python3
+
     which python
     python --version
     which pip
     pip --version
     pip list
-
-    # update pip for newer TLS support
-    #curl https://bootstrap.pypa.io/get-pip.py | ${PYTHONEXE}
-    #"${PYTHONBIN}/pip" install --upgrade pip
 
     # update virtualenv and setuptools
     ${PYTHONEXE} -m pip install virtualenv
