cb87cf3a05ed472eb08051092c3b28e504c55dcb
Chris Conlon <chris@wolfssl.com>
2019-01-23 10:29:26 -0700

    update path

--- make/osx/build_wheels.sh
+++ make/osx/build_wheels.sh
@@ -3,6 +3,7 @@ set +x
 
 for PYVERSION in 2.7 3.4 3.5 3.6; do
     PYTHONEXE=/Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin/python${PYVERSION}
+    export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin
 
     # update pip for newer TLS support
     curl https://bootstrap.pypa.io/get-pip.py | python
