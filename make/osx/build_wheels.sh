fa348a7a2f05b9e6a79136c64d1f7ee2c11c5788
Chris Conlon <chris@wolfssl.com>
2019-01-23 13:08:42 -0700

    trying fix

--- make/osx/build_wheels.sh
+++ make/osx/build_wheels.sh
@@ -3,18 +3,23 @@ set +x
 
 for PYVERSION in 2.7 3.4 3.5 3.6; do
     PYTHONEXE=/Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin/python${PYVERSION}
-    export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin
+    export PATH="/Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin":$PATH
 
     # update pip for newer TLS support
-    curl https://bootstrap.pypa.io/get-pip.py | python
-    pip install --upgrade pip
+    #curl https://bootstrap.pypa.io/get-pip.py | python
+    #pip install --upgrade pip
 
     # update virtualenv and setuptools
-    $PYTHONEXE -m pip install virtualenv
+    python -m pip install virtualenv
+    #$PYTHONEXE -m pip install virtualenv
     #$PYTHONEXE pip install --upgrade setuptools
 
     virtualenv -p /Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin/python${PYVERSION} venv_${PYVERSION}
     . ./venv_${PYVERSION}/bin/activate
+
+    # update pip for newer TLS support
+    curl https://bootstrap.pypa.io/get-pip.py | python
+    pip install --upgrade pip
 
     python setup.py bdist_wheel
     pip install -r requirements/test.txt
