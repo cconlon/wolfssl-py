4b2917eadcbbacebc1e3fa800edb3c7106d4161d
Chris Conlon <chris@wolfssl.com>
2019-01-23 09:53:34 -0700

    another travis fix test

--- make/osx/build_wheels.sh
+++ make/osx/build_wheels.sh
@@ -10,7 +10,7 @@ for PYVERSION in 2.7 3.4 3.5 3.6; do
 
     # update virtualenv and setuptools
     $PYTHONEXE -m pip install virtualenv
-    $PYTHONEXE pip install --upgrade setuptools
+    #$PYTHONEXE pip install --upgrade setuptools
 
     virtualenv -p /Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin/python${PYVERSION} venv_${PYVERSION}
     . ./venv_${PYVERSION}/bin/activate
