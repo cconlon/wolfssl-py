20afc8a59ceed3c066e3584b7b05c5290fc594aa
Chris Conlon <chris@wolfssl.com>
2019-01-23 15:27:51 -0700

    fixes for travis

--- make/manylinux1/build.sh
+++ make/manylinux1/build.sh
@@ -5,10 +5,11 @@ set -e -x
 if [ -d dist ]; then mv dist tmpdist; fi
 
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/python" setup.py bdist_wheel
-    rm -rf .eggs
-done
+#for PYBIN in /opt/python/*/bin; do
+#    "${PYBIN}/python" setup.py bdist_wheel
+#    rm -rf .eggs
+#done
+python setup.py bdist_wheel
 
 # Bundle external shared libraries into the wheels
 ls dist/ | grep wolfssl
@@ -20,9 +21,14 @@ done
 rm -rf dist && mv tmpdist dist
 
 # Install packages and test
-for PYBIN in /opt/python/*/bin/; do
-    "${PYBIN}/pip" install -r requirements/test.txt
-    "${PYBIN}/pip" install wolfssl --no-index -f dist
-    rm -rf tests/__pycache__
-    "${PYBIN}/py.test" tests
-done
+#for PYBIN in /opt/python/*/bin/; do
+#    "${PYBIN}/pip" install -r requirements/test.txt
+#    "${PYBIN}/pip" install wolfssl --no-index -f dist
+#    rm -rf tests/__pycache__
+#    "${PYBIN}/py.test" tests
+#done
+pip install -r requirements/test.txt
+pip install wolfssl --no-index -f dist
+rm -rf tests/__pycache__
+py.test tests
+
--- make/osx/build_wheels.sh
+++ make/osx/build_wheels.sh
@@ -1,21 +1,31 @@
 set -e
 set +x
 
-# update pip for newer TLS support
-curl https://bootstrap.pypa.io/get-pip.py | python
-pip install --upgrade pip
+for PYVERSION in 2.7 3.4 3.5 3.6; do
+    PYTHONBIN=/Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin
+    PYTHONEXE=${PYTHONBIN}/python${PYVERSION}
+    export PATH="${PYTHONBIN}":$PATH
 
-# update virtualenv
-python -m pip install virtualenv
+    # update pip for newer TLS support
+    curl https://bootstrap.pypa.io/get-pip.py | ${PYTHONEXE}
+    "${PYTHONBIN}/pip" install --upgrade pip
 
-# install cffi module
-python -m pip install cffi
+    # update virtualenv
+    ${PYTHONEXE} -m pip install virtualenv
 
-python setup.py bdist_wheel
-pip install -r requirements/test.txt
-set +e
-pip uninstall -y wolfssl
-set -e
-pip install wolfssl --no-index -f dist
-rm -rf tests/__pycache__
-py.test tests
+    virtualenv -p ${PYTHONEXE} venv_${PYVERSION}
+    . ./venv_${PYVERSION}/bin/activate
+
+    # install cffi module
+    ${PYTHONEXE} -m pip install cffi
+
+    ${PYTHONEXE} setup.py bdist_wheel
+    "${PYTHONBIN}/pip" install -r requirements/test.txt
+    set +e
+    "${PYTHONBIN}/pip" uninstall -y wolfssl
+    set -e
+    "${PYTHONBIN}/pip" install wolfssl --no-index -f dist
+    rm -rf tests/__pycache__
+    py.test tests
+    deactivate
+done
