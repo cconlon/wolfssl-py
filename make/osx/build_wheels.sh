40e7cc6a570db748c9da65be186e74da892cd05e
Chris Conlon <chris@wolfssl.com>
2019-01-23 13:13:22 -0700

    rearrange travis file

--- make/osx/build_wheels.sh
+++ make/osx/build_wheels.sh
@@ -6,8 +6,8 @@ for PYVERSION in 2.7 3.4 3.5 3.6; do
     export PATH="/Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin":$PATH
 
     # update pip for newer TLS support
-    #curl https://bootstrap.pypa.io/get-pip.py | python
-    #pip install --upgrade pip
+    curl https://bootstrap.pypa.io/get-pip.py | python
+    pip install --upgrade pip
 
     # update virtualenv and setuptools
     python -m pip install virtualenv
@@ -18,8 +18,8 @@ for PYVERSION in 2.7 3.4 3.5 3.6; do
     . ./venv_${PYVERSION}/bin/activate
 
     # update pip for newer TLS support
-    curl https://bootstrap.pypa.io/get-pip.py | python
-    pip install --upgrade pip
+    #curl https://bootstrap.pypa.io/get-pip.py | python
+    #pip install --upgrade pip
 
     python setup.py bdist_wheel
     pip install -r requirements/test.txt
