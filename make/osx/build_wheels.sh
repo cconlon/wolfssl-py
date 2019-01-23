8e38147bfd59d1261681768d966143caf85c9cac
Chris Conlon <chris@wolfssl.com>
2019-01-23 13:29:34 -0700

    use python from install location on osx travis build

--- make/osx/build_wheels.sh
+++ make/osx/build_wheels.sh
@@ -2,31 +2,28 @@ set -e
 set +x
 
 for PYVERSION in 2.7 3.4 3.5 3.6; do
-    PYTHONEXE=/Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin/python${PYVERSION}
-    export PATH="/Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin":$PATH
+    PYTHONBIN=/Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin
+    PYTHONEXE=${PYTHONBIN}/python${PYVERSION}
+    export PATH="${PYTHONBIN}":$PATH
 
     # update pip for newer TLS support
-    curl https://bootstrap.pypa.io/get-pip.py | python
-    pip install --upgrade pip
+    curl https://bootstrap.pypa.io/get-pip.py | ${PYTHONEXE}
+    "${PYTHONBIN}/pip" install --upgrade pip
 
     # update virtualenv and setuptools
-    python -m pip install virtualenv
+    ${PYTHONEXE} -m pip install virtualenv
     #$PYTHONEXE -m pip install virtualenv
     #$PYTHONEXE pip install --upgrade setuptools
 
-    virtualenv -p /Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin/python${PYVERSION} venv_${PYVERSION}
+    virtualenv -p ${PYTHONEXE} venv_${PYVERSION}
     . ./venv_${PYVERSION}/bin/activate
 
-    # update pip for newer TLS support
-    curl https://bootstrap.pypa.io/get-pip.py | python
-    pip install --upgrade pip
-
-    python setup.py bdist_wheel
-    pip install -r requirements/test.txt
+    ${PYTHONEXE} setup.py bdist_wheel
+    "${PYTHONBIN}/pip" install -r requirements/test.txt
     set +e
-    pip uninstall -y wolfssl
+    "${PYTHONBIN}/pip" uninstall -y wolfssl
     set -e
-    pip install wolfssl --no-index -f dist
+    "${PYTHONBIN}/pip" install wolfssl --no-index -f dist
     rm -rf tests/__pycache__
     py.test tests
     deactivate
