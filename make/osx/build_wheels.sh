4133fa40e70562ab06f0238dbee7fdcecadfdbac
Chris Conlon <chris@wolfssl.com>
2019-01-23 09:32:26 -0700

    travis ci fixes

--- make/osx/build_wheels.sh
+++ make/osx/build_wheels.sh
@@ -4,9 +4,14 @@ set +x
 for PYVERSION in 2.7 3.4 3.5 3.6; do
     virtualenv -p /Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin/python${PYVERSION} venv_${PYVERSION}
     . ./venv_${PYVERSION}/bin/activate
+
+    # update pip
     curl https://bootstrap.pypa.io/get-pip.py | python
     pip install --upgrade pip
+
+    # update setuptools
     pip install --upgrade setuptools
+
     python setup.py bdist_wheel
     pip install -r requirements/test.txt
     set +e
